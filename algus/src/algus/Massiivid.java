9cc3b4ef17f8a166a077f43f2db1f9350befd4cb
minak <minak@Kartuke>
2018-02-01 10:40:44 +0200

    Midagi muud.

---
+++ algus/src/algus/Massiivid.java
@@ -0,0 +1,22 @@
+
+package algus;
+
+public class Massiivid{
+    public static void main(String[] arg){
+     int[] pikkused={176, 163, 158, 171, 169}; 
+     System.out.println("Kokku: "+pikkused.length);
+     System.out.println(pikkused[0]+" "+pikkused[pikkused.length-1]);
+     for(int i=0; i<pikkused.length; i++){
+        System.out.println(pikkused[i]);
+     }
+     //arvuta pikkuste summa
+     int summa=0;
+     for(int i=0; i<pikkused.length; i++){
+        System.out.println(pikkused[i]);
+    }
+      System.out.println(summa);
+      summa=0;
+      for(int pikkus: pikkused){summa+=pikkus;}
+      System.out.println(summa);
+      System.out.println(Arrays.stream(pikkused).sum());
+}
