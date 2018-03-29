b444ed7f21296dd3164448e9bd3bc655993e5811
minak <minak@Kartuke>
2018-03-29 10:42:22 +0300

    lisatud testid

--- Liidesed/src/test/java/liidesed/AlaTestid.java
+++ Liidesed/src/test/java/liidesed/AlaTestid.java
@@ -55,5 +55,21 @@ public class AlaTestid {
                "..........\n")
        );
     }
-
+    @Test public void sobivKoht(){
+        assertTrue(a.kasVaba(3, 2));
+        assertTrue(a.kasVaba(9, 9));
+        assertFalse(a.kasVaba(9, 10));
+        assertFalse(a.kasVaba(10, 9));
+        assertFalse(a.kasVaba(0, 0));
+        assertFalse(a.kasVaba(-1, 0));
+         assertFalse(a.kasVaba(0, -1));
+    }
+    @Test public void vaiksemAlaKohad(){
+        Ala a2= new Ala(3,6);
+        a2.k2sud("epee");
+        assertTrue(a2.kasVaba(2, 5));
+        assertFalse(a2.kasVaba(5, 2));
+        assertTrue(a2.kasVaba(0, 0));
+        assertFalse(a2.kasVaba(1, 2));
+    }
 }
